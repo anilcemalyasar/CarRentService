61d7f51378e053a27b7ab887d6caab4c6705dc78
Anil Yasar <anlysr2002@gmail.com>
2023-10-25 00:03:53 +0300

    project created

---
+++ service/service/src/main/java/com/carrental/service/business/StorageService.java
@@ -0,0 +1,42 @@
+package com.carrental.service.business;
+
+import com.carrental.service.model.entity.ImageData;
+import com.carrental.service.repository.StorageRepository;
+import com.carrental.service.util.ImageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Service
+public class StorageService {
+
+    private StorageRepository storageRepository;
+
+    @Autowired
+    public StorageService(StorageRepository storageRepository) {
+        this.storageRepository = storageRepository;
+    }
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        ImageData imageData = storageRepository.save(ImageData.builder()
+                .name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .imageData(ImageUtils.compressImage(file.getBytes())).build()
+        );
+
+        if (imageData != null) {
+            return "File uploaded successfully : " + file.getOriginalFilename();
+        }
+        return null;
+
+    }
+
+    public byte[] downloadImage(String fileName) {
+        Optional<ImageData> dbImageData = storageRepository.findByName(fileName);
+        byte[] images = ImageUtils.decompressImage(dbImageData.get().getImageData());
+        return images;
+    }
+}
---
+++ service/service/src/main/java/com/carrental/service/model/entity/ImageData.java
@@ -0,0 +1,29 @@
+package com.carrental.service.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ImageData")
+@Builder
+public class ImageData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String type;
+
+    @Lob
+    @Column(name = "imagedata", length = 1000)
+    private byte[] imageData;
+
+
+}
